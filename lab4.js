8ae5cec16c7f7fc57e400468283c839a7392c00c
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-06-05 01:38:41 +0300

    first commit

---
+++ lab4.js
@@ -0,0 +1,271 @@
+function countDegrees(matrix) {
+  const degrees = {};
+  for (let i = 0; i < qntnNodes; i++) {
+    degrees[i] = { degree: 0, inDegree: 0, outDegree: 0 };
+  }
+
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] === 1) {
+        degrees[i].degree++;
+        degrees[j].degree++;
+        degrees[i].outDegree++;
+        degrees[j].inDegree++;
+      }
+    }
+  }
+
+  return degrees;
+}
+
+function isGraphRegular(degrees) {
+  const firstDegree = degrees[0].degree;
+  for (const node in degrees) {
+    if (degrees[node].degree !== firstDegree) {
+      return console.log("Graph is not regular");
+    }
+  }
+  return console.log(
+    "Graph is regular, degree of regularity is " + firstDegree
+  );
+}
+
+function printHangingAndIsolatedNodes(degrees) {
+  const hangingNodes = [];
+  const isolatedNodes = [];
+
+  for (const node in degrees) {
+    if (degrees[node].degree === 1) {
+      hangingNodes.push(node);
+    } else if (degrees[node].degree === 0) {
+      isolatedNodes.push(node);
+    }
+  }
+
+  console.log(
+    "Hanging vertices:",
+    hangingNodes.length > 0 ? hangingNodes : "There is none"
+  );
+  console.log(
+    "Isolated vertices:",
+    isolatedNodes.length > 0 ? isolatedNodes : "There is none"
+  );
+}
+
+function multiplyMatrices(matrix1, matrix2) {
+  const multipliedMatrices = [];
+  for (let i = 0; i < matrix1.length; i++) {
+    multipliedMatrices[i] = [];
+    for (let j = 0; j < matrix2[0].length; j++) {
+      multipliedMatrices[i][j] = 0;
+      for (let k = 0; k < matrix1[0].length; k++) {
+        multipliedMatrices[i][j] += matrix1[i][k] * matrix2[k][j];
+      }
+    }
+  }
+  return multipliedMatrices;
+}
+
+function findPathsLength2(matrix) {
+  const paths = [];
+
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] != 0) {
+        for (let k = 0; k < qntnNodes; k++) {
+          if (matrix[j][k] != 0) {
+            paths.push([i, j, k]);
+          }
+        }
+      }
+    }
+  }
+
+  return paths;
+}
+
+function findPathsLength3(matrix) {
+  const paths = [];
+
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] != 0) {
+        for (let k = 0; k < qntnNodes; k++) {
+          if (matrix[j][k] != 0) {
+            for (let l = 0; l < qntnNodes; l++) {
+              if (matrix[k][l] != 0) {
+                paths.push([i, j, k, l]);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return paths;
+}
+
+function countPaths(matrix) {
+  const A2 = multiplyMatrices(matrix, matrix);
+  const A3 = multiplyMatrices(A2, matrix);
+
+  console.log("Matrix A2 : ", A2);
+  console.log("Matrix A3 : ", A3);
+
+  console.log("Paths of length 2:");
+  const pathsLength2 = findPathsLength2(matrix);
+  pathsLength2.forEach((path) =>
+    console.log(path.map((v) => v + 1).join(" -> "))
+  );
+
+  console.log("Paths of length 3:");
+  const pathsLength3 = findPathsLength3(matrix);
+  pathsLength3.forEach((path) =>
+    console.log(path.map((v) => v + 1).join(" -> "))
+  );
+}
+
+function reachabilityMatrix(adjacencyMatrix) {
+  let reachability = Array.from({ length: qntnNodes }, () =>
+    Array(qntnNodes).fill(0)
+  );
+
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (adjacencyMatrix[i][j] === 1) {
+        reachability[i][j] = 1;
+      }
+    }
+  }
+
+  for (let k = 0; k < qntnNodes; k++) {
+    for (let i = 0; i < qntnNodes; i++) {
+      for (let j = 0; j < qntnNodes; j++) {
+        if (reachability[i][k] && reachability[k][j]) {
+          reachability[i][j] = 1;
+        }
+      }
+    }
+  }
+
+  return reachability;
+}
+
+function strongComponents(adjacencyMatrix) {
+  let index = 0;
+  const stack = [];
+  const visited = new Array(qntnNodes).fill(-1);
+  const min = new Array(qntnNodes).fill(-1);
+  const inStack = new Array(qntnNodes).fill(false);
+  const components = [];
+
+  function findComp(v) {
+    visited[v] = min[v] = index++;
+    stack.push(v);
+    inStack[v] = true;
+
+    for (let w = 0; w < qntnNodes; w++) {
+      if (adjacencyMatrix[v][w] !== 0) {
+        if (visited[w] === -1) {
+          findComp(w);
+          min[v] = Math.min(min[v], min[w]);
+        } else if (inStack[w]) {
+          min[v] = Math.min(min[v], visited[w]);
+        }
+      }
+    }
+
+    if (min[v] === visited[v]) {
+      const component = [];
+      let w;
+      do {
+        w = stack.pop();
+        inStack[w] = false;
+        component.push(w);
+      } while (w !== v);
+      components.push(component);
+    }
+  }
+
+  for (let i = 0; i < qntnNodes; i++) {
+    if (visited[i] === -1) {
+      findComp(i);
+    }
+  }
+
+  return components;
+}
+
+function createStrongConnectivityMatrix(adjacencyMatrix, components) {
+  const n = components.length;
+  const componentIndex = new Map();
+
+  components.forEach((component, index) => {
+    component.forEach((vertex) => {
+      componentIndex.set(vertex, index);
+    });
+  });
+
+  const strongConnectivityMatrix = Array.from({ length: n }, () =>
+    Array(n).fill(0)
+  );
+
+  for (let v = 0; v < adjacencyMatrix.length; v++) {
+    for (let w = 0; w < adjacencyMatrix[v].length; w++) {
+      if (adjacencyMatrix[v][w] !== 0) {
+        const vComponent = componentIndex.get(v);
+        const wComponent = componentIndex.get(w);
+        if (vComponent !== wComponent) {
+          strongConnectivityMatrix[vComponent][wComponent] = 1;
+        }
+      }
+    }
+  }
+
+  return strongConnectivityMatrix;
+}
+
+const components = strongComponents(matrixNotSymmetrical);
+console.log("Components of Strong Connectivity:");
+components.forEach((component, index) => {
+  console.log(`Component ${index + 1}: ${component}`);
+});
+
+const degreesSymmetrical = countDegrees(matrixSymmetrical);
+console.log("Degrees of symmetrical matrix : ");
+console.log(degreesSymmetrical);
+isGraphRegular(degreesSymmetrical);
+printHangingAndIsolatedNodes(degreesSymmetrical);
+countPaths(matrixSymmetrical);
+let reachabilitySymmetrical = reachabilityMatrix(matrixNotSymmetrical);
+console.log(reachabilitySymmetrical);
+
+const degreesNotSymmetrical = countDegrees(matrixNotSymmetrical);
+console.log("Degrees of not symmetrical matrix : ");
+console.log(degreesNotSymmetrical);
+isGraphRegular(degreesNotSymmetrical);
+printHangingAndIsolatedNodes(degreesNotSymmetrical);
+countPaths(matrixNotSymmetrical);
+let reachabilityNotSymmetrical = reachabilityMatrix(matrixNotSymmetrical);
+console.log(reachabilityNotSymmetrical);
+
+const strongConnectivityMatrix = createStrongConnectivityMatrix(
+  matrixNotSymmetrical,
+  components
+);
+console.log("Strong Connectivity Matrix:");
+console.log(strongConnectivityMatrix);
+
+qntnNodes = strongConnectivityMatrix.length;
+
+const nodePositionsCond = Array.from({length: qntnNodes}, (_, i) => {
+  const angle = (Math.PI * 2 * i) / qntnNodes - Math.PI/2; 
+  return {
+    x: centerX + graphRadius * Math.cos(angle),
+    y: centerY + graphRadius * Math.sin(angle)
+  };
+});
+
+drawAllEdges(strongConnectivityMatrix, contextCond, nodePositionsCond);
+drawAllNodes(contextCond, "#e272c6", "black", nodePositionsCond);
---
+++ script.js
@@ -0,0 +1,231 @@
+const canvasNeNapr = document.getElementById("neNapr");
+const canvasNapr = document.getElementById("napr");
+const contextNeNapr = canvasNeNapr.getContext("2d");
+const contextNapr = canvasNapr.getContext("2d");
+
+const canvasCond = document.getElementById("condGraph");
+const contextCond = canvasCond.getContext("2d");
+
+let qntnNodes = 12;
+const coef1 = 1.0 - 2 * 0.01 - 1 * 0.01 - 0.3;
+const coef2 = 1.0 - 2 * 0.005 - 1 * 0.005 - 0.27;
+const radius = 15;
+
+const graphRadius = 200; 
+const centerX = 300;    
+const centerY = 300;    
+
+const nodePositions = Array.from({length: qntnNodes}, (_, i) => {
+  const angle = (Math.PI * 2 * i) / qntnNodes - Math.PI/2; 
+  return {
+    x: centerX + graphRadius * Math.cos(angle),
+    y: centerY + graphRadius * Math.sin(angle)
+  };
+});
+
+
+function generateAdjacencyMatrixSymmetrical(coef) {
+  const seed = 4321;
+  let matrix = [];
+  Math.seedrandom(seed);
+  for (let i = 0; i < qntnNodes; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < qntnNodes; j++) {
+      matrix[i][j] = Math.random() * 2 * coef1;
+      matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
+    }
+  }
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] === 1 && matrix[i][j] !== matrix[j][i]) {
+        matrix[j][i] = 1;
+      }
+    }
+  }
+  return matrix;
+}
+
+function generateAdjacencyMatrixNotSymmetrical(coef) {
+  const seed = 4321;
+  let matrix = [];
+  Math.seedrandom(seed);
+  for (let i = 0; i < qntnNodes; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < qntnNodes; j++) {
+      matrix[i][j] = Math.random() * 2 * coef;
+      matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
+    }
+  }
+  return matrix;
+}
+
+function drawAllNodes(context, colour, txtColour, neededNodePositions) {
+  neededNodePositions.forEach((position, index) => {
+    if (index < qntnNodes) {
+      context.fillStyle = colour;
+      context.strokeStyle = "black";
+      context.lineWidth = 2;
+      context.beginPath();
+      context.arc(position.x, position.y, radius, 0, Math.PI * 2, true);
+      context.fill();
+      context.stroke();
+      context.font = "14px Times New Roman";
+      context.fillStyle = txtColour;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(`${index + 1}`, position.x, position.y);
+    }
+  });
+}
+
+function drawNode(index, context, bgColour, txtColour) {
+  context.fillStyle = bgColour;
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+
+  context.beginPath();
+  context.arc(
+    nodePositions[index].x,
+    nodePositions[index].y,
+    radius,
+    0,
+    Math.PI * 2,
+    true
+  );
+  context.fill();
+  context.stroke();
+  context.font = "14px Times New Roman";
+  context.fillStyle = txtColour;
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+  context.fillText(
+    `${index + 1}`,
+    nodePositions[index].x,
+    nodePositions[index].y
+  );
+}
+
+const drawArrow = (x, y, context, angle, size) => {
+  const arrowSize = size;
+
+  context.save();
+  context.translate(x, y);
+  context.rotate(angle);
+  context.moveTo(0, 0);
+  context.lineTo(arrowSize, -arrowSize);
+  context.lineTo(0, 0);
+  context.lineTo(arrowSize, arrowSize);
+  context.closePath();
+  context.restore();
+};
+
+function drawEdgeArrow(fromNode, toNode, context, neededNodePositions) {
+  const startX = neededNodePositions[fromNode].x;
+  const startY = neededNodePositions[fromNode].y;
+  const endX = neededNodePositions[toNode].x;
+  const endY = neededNodePositions[toNode].y;
+  const angle = Math.atan2(startY - endY, startX - endX);
+  const indentX = radius * Math.cos(angle);
+  const indentY = radius * Math.sin(angle);
+  drawArrow(endX + indentX, endY + indentY, context, angle, 4);
+}
+
+function drawEdge(
+  fromNode,
+  toNode,
+  context,
+  colour,
+  lineWidth,
+  neededNodePositions
+) {
+  const startX = neededNodePositions[fromNode].x;
+  const startY = neededNodePositions[fromNode].y;
+  const endX = neededNodePositions[toNode].x;
+  const endY = neededNodePositions[toNode].y;
+  context.strokeStyle = colour;
+  context.lineWidth = lineWidth;
+  context.beginPath();
+  context.moveTo(startX, startY);
+  context.lineTo(endX, endY);
+  context.stroke();
+  context.closePath();
+}
+
+function drawAllEdges(matrix, context, neededNodePositions) {
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] === 1) {
+        if (neededNodePositions === nodePositions) {
+          drawEdge(i, j, context, "black", 2, nodePositions);
+        } else if (neededNodePositions === nodePositionsCond) {
+          drawEdge(i, j, context, "black", 2, nodePositionsCond);
+        }
+
+        if (context === contextNapr || context === contextCond) {
+          if (neededNodePositions === nodePositions) {
+            drawEdgeArrow(i, j, context, nodePositions);
+          } else if (neededNodePositions === nodePositionsCond) {
+            drawEdgeArrow(i, j, context, nodePositionsCond);
+          }
+        }
+        if (i === j) {
+          context.beginPath();
+          const endX = neededNodePositions[j].x;
+          const endY = neededNodePositions[j].y;
+          if (i < 5) {
+            context.arc(
+              endX,
+              endY - radius * 2,
+              radius,
+              -Math.PI / 2,
+              (3 * Math.PI) / 2,
+              true
+            );
+            if (context === contextNapr || context === contextCond) {
+              drawArrow(
+                neededNodePositions[i].x + 5,
+                neededNodePositions[i].y - 15,
+                context,
+                -Math.PI / 4,
+                4
+              );
+            }
+            context.stroke();
+          } else {
+            context.arc(
+              endX,
+              endY + radius * 2,
+              radius,
+              Math.PI / 2,
+              (-3 * Math.PI) / 2,
+              true
+            );
+            if (context === contextNapr || context === contextCond) {
+              drawArrow(
+                neededNodePositions[i].x + 5,
+                neededNodePositions[i].y + 15,
+                context,
+                Math.PI / 4,
+                4
+              );
+            }
+            context.stroke();
+          }
+        }
+        context.stroke();
+      }
+    }
+  }
+}
+
+const matrixSymmetrical = generateAdjacencyMatrixSymmetrical(coef1);
+console.log("Symmetrical matrix : ", matrixSymmetrical);
+
+const matrixNotSymmetrical = generateAdjacencyMatrixNotSymmetrical(coef1);
+console.log("Not symmetrical matrix : ", matrixNotSymmetrical);
+
+drawAllEdges(matrixNotSymmetrical, contextNapr, nodePositions);
+drawAllEdges(matrixSymmetrical, contextNeNapr, nodePositions);
+
+drawAllNodes(contextNeNapr, "#e272c6", "black", nodePositions);
+drawAllNodes(contextNapr, "#e272c6", "black", nodePositions);
